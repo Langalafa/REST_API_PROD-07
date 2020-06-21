2672372b30a01a3e702e8327ba52768b5c2067b0
Langa <langalafa@gmail.com>
2020-06-21 01:08:33 -0500

    hexal-lambda-functions-master

--- hexalDelete.js
+++ hexalDelete.js
@@ -1,1 +1,41 @@
+'use strict';
+const AWS = require('aws-sdk');
 
+AWS.config.update({ region: "us-east-1"});
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1"});
+
+  let responseBody = "";
+  let statusCode = 0;
+   
+ const {id} = event.pathParameters;
+
+  const params = { 
+    TableName: "Products",
+    Item: {
+      id
+    }
+  };
+
+  try {
+    const data = await documentClient.delete(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 204;
+  } catch(err) {
+    responseBody = `Unable to delete product: ${err}`;
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+       "access-control-allow-orrigin": "*"
+    },
+    body: responseBody
+  };
+
+  return response;
+};
